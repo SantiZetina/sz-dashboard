db4b1d8bdb6ec76fe56db928d75d6ad116368982
Santiago Zetina <zetinasanti@gmail.com>
2024-01-27 17:19:47 -0500

    Add typing effect to welcome message

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -11,7 +11,7 @@ export default function WelcomePage() {
             <div className="flex flex-col justify-center items-center h-screen">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold">Welcome</h1>
                
-                <h3>Welcome to my page my name is</h3>
+                <h3 className='typing-effect'>Welcome to my page my name is Santiago</h3>
                 
             </div>
         </RootLayout>
