45568b9c6fdb99a377a7f6b4cf89118ef25c326c
Santiago Zetina <zetinasanti@gmail.com>
2024-01-28 20:15:26 -0500

    Update global CSS and add typing effect to welcome page

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,15 +1,16 @@
-import Navbar from "@/components/ui/navbar";
 import RootLayout from "../layout";
-import { Calendar } from "@/components/ui/calendar";
-import React from "react";
  // Import the useClient hook
+ 
 
 export default function WelcomePage() {
 
     return (
         <RootLayout>
             <div className="flex flex-col justify-center items-center h-screen">
-                <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold">Welcome</h1>
+                <h1 className="text-4xl sm:text-5xl md:text-8xl font-bold">Welcome</h1>
+               <div className="typing-container">
+                <h3 className='typing-effect'>Welcome to my page my name is Santiago</h3>
+                </div>
             </div>
         </RootLayout>
     )
