c87c171be48791de7354aa2b41db37431d716d6a
Santiago Zetina <zetinasanti@gmail.com>
2024-01-30 16:49:04 -0500

    Update width of typing container and modify welcome page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
-import { Alert, AlertDescription, AlertTitle} from '../components/ui/alert';
-import RootLayout from './layout';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import WelcomePage from './welcome/page';
+// import { Alert, AlertDescription, AlertTitle} from '../components/ui/alert';
+// import RootLayout from './layout';
+// import Link from 'next/link';
+// import { Button } from '@/components/ui/button';
+// import WelcomePage from './welcome/page';
 
 
 
 export default function Page() {
   return (
-    <div className='flex min-h-screen flex-col p-6'>
-    <div className='flex flex-col items-center justify-center md:justify-center 
-    text-center gap-y-8 flex-1 px-6 pb-10'>
-     
-      <WelcomePage />
-    </div>
-    </div>
+    <main className='flex min-h-screen flex-col p-4 sm:p-6'>
+      <div className='flex flex-col items-center justify-center md:justify-center 
+        text-center gap-y-8 flex-1 px-4 sm:px-6 pb-10'>
+                <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold">Welcome</h1>
+        <div className="typing-container">
+          <h3 className='typing-effect text-sm sm:text-md md:text-lg'>Welcome to my page, my name is Santiago</h3>
+        </div>
+      </div>
+    </main>
   )
 }
