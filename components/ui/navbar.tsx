1e877ebcd47a0135833df18d88a8ce8388646a65
Santiago Zetina <zetinasanti@gmail.com>
2024-01-30 19:54:47 -0500

    Add sticky positioning to navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="flex flex-col items-center sm:flex-row sm:justify-between p-4 bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-100 shadow-md">
+    <nav className="sticky top-0 z-50 flex flex-col items-center sm:flex-row sm:justify-between p-4 bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-100 shadow-md">
       <div className="flex justify-end items-center w-full sm:w-auto">
         <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="p-2 mr-2 sm:hidden">
           <HamburgerMenuIcon className="h-6 w-6 text-gray-600 dark:text-gray-300" />
