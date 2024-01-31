c5354706c8e6b1e46599b6fde149f0df0632469d
Santiago Zetina <zetinasanti@gmail.com>
2024-01-31 17:05:57 -0500

    Refactor work page layout

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -3,7 +3,22 @@ import RootLayout from "../layout";
 export default function WorkPage() {
     return (
         <RootLayout>
-        <h1>Work</h1>
+            <div className="container mx-auto px-4">
+                <h1 className="text-3xl font-bold text-center my-8">
+                    My Work
+                </h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {/* Sample project cards */}
+                    {[1, 2, 3, 4, 5, 6].map((item) => (
+                        <div key={item} className="border rounded-lg p-4 shadow hover:shadow-lg transition-all">
+                            <h2 className="text-xl font-semibold">Project {item}</h2>
+                            <p className="text-gray-600">Description of project {item}...</p>
+                            {/* Add more details like technologies used, link to project, etc. */}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </RootLayout>
-    )
-}  
+    );
+}
+
