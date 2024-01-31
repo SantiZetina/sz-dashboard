c9482a362065eab61c81780b1c8a014701181e0e
Santiago Zetina <zetinasanti@gmail.com>
2024-01-30 19:02:16 -0500

    Refactor navbar component to add responsive menu

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,20 +1,30 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 
 export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="flex justify-center space-x-4">
-      <Link href="/" className="text-blue-500 hover:text-blue-700">
-        Home
-      </Link>
-      <Link href="/about" className="text-blue-500 hover:text-blue-700">
-        About
-      </Link>
-      <Link href="/projects" className="text-blue-500 hover:text-blue-700">
-        Projects
-      </Link>
-      <Link href="/contact" className="text-blue-500 hover:text-blue-700">
-        Contact
-      </Link>
+    <nav className="flex flex-col items-center sm:flex-row sm:justify-center">
+      <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="sm:hidden">
+        {/* Hamburger Icon */}
+      </button>
+      <div className={`flex flex-col sm:flex-row ${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
+        <Link href="/" className="text-blue-500 hover:text-blue-700">
+            Home
+        </Link>
+        <Link href="/about" className="text-blue-500 hover:text-blue-700">
+            About
+        </Link>
+        <Link href="/projects" className="text-blue-500 hover:text-blue-700">
+            Projects
+        </Link>
+        <Link href="/contact" className="text-blue-500 hover:text-blue-700">
+            Contact
+        </Link>
+      </div>
     </nav>
   );
 }
