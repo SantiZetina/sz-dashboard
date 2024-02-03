0ad430ab4f15cc1599a1be01c717cd469351d5b6
Santiago Zetina <zetinasanti@gmail.com>
2024-02-03 16:36:50 -0500

    Update AboutPage to ContactPage

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,18 +3,18 @@ import RootLayout from "../layout";
 export default function ContactPage() {
     return (
         <RootLayout>
-            <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 bg-gray-100 dark:bg-gray-900 text-center">
-                <h1 className="text-4xl font-bold text-gray-800 dark:text-white sm:text-5xl">
+            <div className="flex flex-col items-center justify-center min-h-screen px-4 sm:px-6 pb-24 text-center gap-y-8 bg-background text-foreground">
+                <h1 className="text-3xl sm:text-6xl md:text-8xl lg:text-8xl font-bold">
                     Contact Me
                 </h1>
-                <p className="mt-4 text-lg text-gray-600 dark:text-gray-300 sm:mt-6 sm:text-xl">
+                <p className="text-sm sm:text-md md:text-lg">
                     Feel free to reach out for collaborations or just a friendly hello!
                 </p>
-                <div className="mt-8">
+                <div className="max-w-full mx-auto">
                     {/* Contact form or contact information here */}
-                    {/* You can later add a form with input fields for name, email, message, etc. */}
                 </div>
             </div>
         </RootLayout>
     );
 }
+
